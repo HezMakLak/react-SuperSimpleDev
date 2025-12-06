ff6689bb213044b95f7f180c85de7a5bec467e04
HezMakLak <hezekiah.mekonen@yahoo.com>
2025-12-07 00:20:42 +0300

    adding features to chatbot - installing dayjs

--- lesson-05/chatbot-app/src/App.jsx
+++ lesson-05/chatbot-app/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Chatbot } from "supersimpledev";
 import { ChatInput } from "./components/ChatInput";
 import ChatMessages from "./components/ChatMessages";
 import "./App.css";
@@ -10,26 +11,39 @@ function App() {
       message: "hello chatbot",
       sender: "user",
       id: "id1",
+      time: 1736127288920,
     },
     {
       message: "Hello! How can I help you?",
       sender: "robot",
       id: "id2",
+      time: 1736127291230,
     },
     {
       message: "can you get me todays date?",
       sender: "user",
       id: "id3",
+      time: 1736127385356,
     },
     {
       message: "Today is September 27",
       sender: "robot",
       id: "id4",
+      time: 1736127385500,
     },
   ]);
   // const [chatMessages, setChatMessages] = array;
   // const chatMessages = array[0];
   // const setChatMessages = array[1];
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      'goodbye': 'Goodbye! Have a great day!',
+      'give me a unique id': function () {
+        return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
+      }
+    })
+  }, []);
 
   return (
     <div className="app-container">
